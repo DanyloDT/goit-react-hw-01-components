82f15df0fc70162ae98b26361ba3ab11f97f205b
Danylo Diachenko <danylodiachenko@gmail.com>
2023-06-23 19:52:01 +0300

    додає стилі і розмітку

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import css from './FriendListItem.module.css';
+
+export const FriendListItem = props => {
+  const { avatar, name, isOnline } = props;
+  return (
+    <li className={css.item}>
+      <span
+        className={`${css.status} ${
+          isOnline ? `${css.online}` : `${css.offline}`
+        }`}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table class={css.transaction_history}>
+      <thead className={css.tabHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tabBody} key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
